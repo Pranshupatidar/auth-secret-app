9a751fb20c04ebfc8f669df5fc938d715919c6eb
Pranshupatidar <patidar.pranshu18@gmail.com>
2021-05-21 15:01:11 +0530

    Level 3 - Added environment variables

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -22,7 +22,8 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret  = "Thisisourlittlesecret";
+const secret  = process.env.SECRET;
+
 userSchema.plugin(encrypt, {secret: secret, encryptedFields : ["password"]});
 
 const User = new mongoose.model("User",userSchema);
